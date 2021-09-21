15a2d237c7eb00d6ae63c5d5c56983ae6bc21965
7ham0 <giorgi9609@gmail.com>
2021-09-21 18:14:16 +0400

    some progress

--- ToDoList/Supportive Files/Extensions/UINavigationController.swift
+++ ToDoList/Supportive Files/Extensions/UINavigationController.swift
@@ -9,15 +9,28 @@ import UIKit
 
 extension UINavigationController {
     public func setupDefaultAppearance(){
-        navigationBar.setBackgroundImage(UIImage(), for: .default)
-        navigationBar.shadowImage = UIImage()
-        navigationBar.backgroundColor = .clear
-        navigationBar.isTranslucent = false
-        navigationBar.barTintColor = UIColor(named: "NavBar_Color")
-        navigationBar.tintColor = UIColor.white
-        navigationBar.titleTextAttributes = [ NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Bold", size: 18.0)!,
-                                              NSAttributedString.Key.foregroundColor: UIColor.white]
-        self.setNeedsStatusBarAppearanceUpdate()
+        if #available(iOS 15.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = UIColor(named: "NavBar_Color")
+            appearance.titleTextAttributes = [ NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Bold", size: 18.0)!,
+                                               NSAttributedString.Key.foregroundColor: UIColor.white]
+//            appearance.largeTitleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16.5) as Any, NSAttributedString.Key.foregroundColor: UIColor.white]
+            appearance.backgroundImage = UIImage()
+            appearance.shadowImage = UIImage()
+            navigationBar.standardAppearance = appearance
+            navigationBar.scrollEdgeAppearance = appearance
+        } else {
+            self.navigationBar.setBackgroundImage(UIImage(), for: .default)
+            self.navigationBar.shadowImage = UIImage()
+            self.navigationBar.backgroundColor = .clear
+            self.navigationBar.barTintColor = UIColor(named: "NavBar_Color")
+            self.navigationBar.tintColor = UIColor.white
+            self.navigationBar.titleTextAttributes = [ NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Bold", size: 18.0)!,
+                                                  NSAttributedString.Key.foregroundColor: UIColor.white]
+            self.setNeedsStatusBarAppearanceUpdate()
+        }
+        
     }
 }
 
